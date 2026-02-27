b933af4d1739fd59dfe9f758d3e61b0b000505c3
Hasnaoui Nidal <hasnaouinidal308@gmail.com>
2026-02-27 01:49:57 +0000

    Align ecommerce demo wrappers to explicit CSS import paths

--- AURA/pages/demos/Ecommerce01Demo.tsx
+++ AURA/pages/demos/Ecommerce01Demo.tsx
@@ -1,5 +1,5 @@
-import '../../../CATALOG/Ecommerce/E-commerce_01/src/index.css';
-import Ecommerce01App from '../../../CATALOG/Ecommerce/E-commerce_01/src/App';
+import "../../../CATALOG/Ecommerce/E-commerce_01/src/index.css";
+import Ecommerce01App from "../../../CATALOG/Ecommerce/E-commerce_01/src/App";
 
 export default function Ecommerce01Demo() {
   return <Ecommerce01App />;
--- AURA/pages/demos/Ecommerce02Demo.tsx
+++ AURA/pages/demos/Ecommerce02Demo.tsx
@@ -1,5 +1,5 @@
-import '../../../CATALOG/Ecommerce/E-commerce_02/src/index.css';
-import Ecommerce02App from '../../../CATALOG/Ecommerce/E-commerce_02/src/App';
+import "../../../CATALOG/Ecommerce/E-commerce_02/src/index.css";
+import Ecommerce02App from "../../../CATALOG/Ecommerce/E-commerce_02/src/App";
 
 export default function Ecommerce02Demo() {
   return <Ecommerce02App />;
